c190883a48dfe7f9fab3db712e372a1ea7ce109a
javinr112 <javin@game5mobile.com>
2024-02-24 20:49:57 +0700

    added sticker book puzzle to professional projects

--- src/Components/ProfessionalProjects.jsx
+++ src/Components/ProfessionalProjects.jsx
@@ -4,61 +4,72 @@ import BOOM from "../images/logo_boom.png";
 import SamsulEscape from "../images/logo_samsul_escape.png";
 import Diving from "../images/logo_diving.png";
 import VR from "../images/logo_vr.png";
+import StickerBookPuzzle from "../images/logo_sticker.png";
 import React from "react";
 import projects from "../ProfessionalProjects.json";
 import DetailModal from "./DetailModal";
 const ProfessionalProjects = () => {
-	const [open, setOpen] = React.useState(false);
-	const [modalDetail, setModalDetail] = React.useState(
-		projects.SamsulEscape.modal,
-	);
-	const handleOpen = (modalDetail) => {
-		setOpen(true);
-		setModalDetail(modalDetail);
-	};
-	const handleClose = () => setOpen(false);
-	return (
-		<section id="projects" className="bg-dark py-12">
-			<SectionHeader>Professional Projects</SectionHeader>
-			<ProjectsLayout>
-				<Project
-					src={VR}
-					title="VR Arcade Game (Unreleased)"
-					contributions={projects.vrArcade.contributions}
-					onClick={handleOpen.bind(this, projects.vrArcade.modal)}
-					overview="Players can play arcade game such as basketball, bowling, ring tossing,
+    const [open, setOpen] = React.useState(false);
+    const [modalDetail, setModalDetail] = React.useState(
+        projects.SamsulEscape.modal
+    );
+    const handleOpen = (modalDetail) => {
+        setOpen(true);
+        setModalDetail(modalDetail);
+    };
+    const handleClose = () => setOpen(false);
+    return (
+        <section id="projects" className="bg-dark py-12">
+            <SectionHeader>Professional Projects</SectionHeader>
+            <ProjectsLayout>
+                <Project
+                    src={StickerBookPuzzle}
+                    title="Sticker Book Puzzle"
+                    contributions={projects.stickerBookPuzzle.contributions}
+                    onClick={handleOpen.bind(
+                        this,
+                        projects.stickerBookPuzzle.modal
+                    )}
+                    overview="A hyper casual game about placing stickers with lots of level type"
+                />
+                <Project
+                    src={VR}
+                    title="VR Arcade Game (Unreleased)"
+                    contributions={projects.vrArcade.contributions}
+                    onClick={handleOpen.bind(this, projects.vrArcade.modal)}
+                    overview="Players can play arcade game such as basketball, bowling, ring tossing,
 					etc. in VR"
-				/>
-				<Project
-					src={SamsulEscape}
-					title="Samsul Escape (Android)"
-					contributions={projects.SamsulEscape.contributions}
-					onClick={handleOpen.bind(this, projects.SamsulEscape.modal)}
-					overview="Find the hidden key and escape from the house. Don't get caught by the guard"
-				/>
-				<Project
-					src={BOOM}
-					title="BOOM (Android)"
-					contributions={projects.BOOM.contributions}
-					onClick={handleOpen.bind(this, projects.BOOM.modal)}
-					overview="3D Top down shooter. Kill all enemies and unlock new level"
-				/>
-				<Project
-					src={Diving}
-					title="Project Diving (Android)"
-					contributions={projects.diving.contributions}
-					onClick={handleOpen.bind(this, projects.diving.modal)}
-					overview="2D casual game. Find 5 toxic barrels located under the sea. 
+                />
+                <Project
+                    src={SamsulEscape}
+                    title="Samsul Escape (Android)"
+                    contributions={projects.SamsulEscape.contributions}
+                    onClick={handleOpen.bind(this, projects.SamsulEscape.modal)}
+                    overview="Find the hidden key and escape from the house. Don't get caught by the guard"
+                />
+                <Project
+                    src={BOOM}
+                    title="BOOM (Android)"
+                    contributions={projects.BOOM.contributions}
+                    onClick={handleOpen.bind(this, projects.BOOM.modal)}
+                    overview="3D Top down shooter. Kill all enemies and unlock new level"
+                />
+                <Project
+                    src={Diving}
+                    title="Project Diving (Android)"
+                    contributions={projects.diving.contributions}
+                    onClick={handleOpen.bind(this, projects.diving.modal)}
+                    overview="2D casual game. Find 5 toxic barrels located under the sea. 
 					Collect trashes for money and use money for upgrade"
-				/>
-			</ProjectsLayout>
-			<DetailModal
-				open={open}
-				handleClose={handleClose}
-				modalDetail={modalDetail}
-			/>
-		</section>
-	);
+                />
+            </ProjectsLayout>
+            <DetailModal
+                open={open}
+                handleClose={handleClose}
+                modalDetail={modalDetail}
+            />
+        </section>
+    );
 };
 
 export default ProfessionalProjects;
